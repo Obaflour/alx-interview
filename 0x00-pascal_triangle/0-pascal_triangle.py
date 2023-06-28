8a6892c6baa9dbbdc90f113634efc61fa53d31c7
Obafuwa Olayemi <obafuwaolayemi@gmail.com>
2023-06-28 19:47:12 +0000

    olayemi

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,23 +1,20 @@
 #!/usr/bin/python3
 """
-0. Pascal's Triangle 
+0. Pascal's Triangle
 """
 
 
 def pascal_triangle(n):
-    if n <= 0:
-        return []
-
-    triangle = []
-
-    for i in range(n):
-        row = [1] * (i + 1)
-
-        if i > 1:
-            prev_row = triangle[i - 1]
-            for j in range(1, i):
-                row[j] = prev_row[j - 1] + prev_row[j]
-
-        triangle.append(row)
-
-    return triangle
+    """Create a function def pascal_triangle(n): that returns a list of lists
+    of integers representing the Pascal’s triangle of n
+    """
+    res = []
+    if n > 0:
+        for i in range(1, n + 1):
+            level = []
+            C = 1
+            for j in range(1, i + 1):
+                level.append(C)
+                C = C * (i - j) // j
+            res.append(level)
+    return res
