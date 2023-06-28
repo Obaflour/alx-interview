3dc504e99f2896bf9ee6f2da534d3c90aa42ad63
Obafuwa Olayemi <obafuwaolayemi@gmail.com>
2023-06-28 19:39:29 +0000

    olayemi

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,16 +2,16 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
-    triangle = [[1]]
+    triangle = []
 
-    for i in range(1, n):
-        row = [1]
+    for i in range(n):
+        row = [1] * (i + 1)
 
-        for j in range(1, i):
-            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        if i > 1:
+            prev_row = triangle[i - 1]
+            for j in range(1, i):
+                row[j] = prev_row[j - 1] + prev_row[j]
 
-        row.append(1)
         triangle.append(row)
 
     return triangle
-
